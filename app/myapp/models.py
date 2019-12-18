a0eceb96b821fa9545d07875b1755d7cb2991a4f
Hyojin Kwon <raccoonhj33@gmail.com>
2019-12-18 17:27:13 +0900

    191218 choices of CharField

---
+++ app/myapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Person(models.Model):
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large')
+    )
+
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
