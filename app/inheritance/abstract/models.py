7288bad9a28b405e54b967e0f3a70651b3b206d7
Hyojin Kwon <raccoonhj33@gmail.com>
2019-12-23 16:27:36 +0900

    191223 inheritance - multi-table

--- app/inheritance/abstract/models.py
+++ app/inheritance/abstract/models.py
@@ -14,8 +14,18 @@ class CommonInfo(models.Model):
     name = models.CharField(max_length=20)
     age = models.PositiveIntegerField()
 
+    # 상속받은 자식 클래스에서 부모 클래스의 Meta 속성을 가지면서 자신만의 속성을 추가하고 싶다면
+    # <부모 클래스>, Meta를 상속받고, 나머지 피룡한 속성들을 정의
+    # 아래 경우, 부모로부터 ordering 속성을 상속받음
     class Meta:
-        abstract = True
+        # abstract = True
+        # ordering = ['name']
+        verbose_name = '학생'
+        verbose_name_plural = '학생 목록'
+        db_table = 'Abstract_Student_Table'
+
+    def __str__(self):
+        return f'{self.name} (학교: {self.school})'
 
 
 class Student(CommonInfo):
@@ -30,3 +40,19 @@ class Instructor(CommonInfo):
 
     def __str__(self):
         return f'{self.name} (학원: {self.academy})'
+
+
+# be careful with related_name and related_query_name
+class Base(models.Model):
+    m2m = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='%(app_label)s_%(class)s_set')
+
+    class Meta:
+        abstract = True
+
+
+class ChildA(Base):
+    pass
+
+
+class ChildB(Base):
+    pass
